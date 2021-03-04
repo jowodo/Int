da4b2c5bc2afbab584215ad64ca6fdae8229d9d3
pur80a <j.w.dorn@gmx.at>
2021-03-04 10:19:28 +0100

    filtered Ag measurments out

--- Data/I-V/stat.py
+++ Data/I-V/stat.py
@@ -8,35 +8,37 @@ def get_sample_name():
     return name
 def get_conductance(filename):
     n_comments=40
-#    print(filename)
+    print(filename)
     for r in range(n_comments):
         try:
             df=pd.read_csv(filename,"\t",header=r,names=("V","I","i","J","j"))
         except:
             pass
         else:
-            break
-    i=0
-    j=-1
-    dv= (df["V"].iloc[j]-df["V"].iloc[i])
-    di= (df["I"].iloc[j]-df["I"].iloc[i])
-    R = dv/di
-    G = di/dv
-    return np.absolute(G)
+#            break
+            i=0     #first 
+            j=-1    #last
+            dv= (df["V"].iloc[j]-df["V"].iloc[i])
+            di= (df["I"].iloc[j]-df["I"].iloc[i])
+            R = dv/di
+            G = di/dv
+            return np.absolute(G)
+    return 1    # if error with file reading
 def get_header():
     return "exp\tconductance [S]"
 def get_Gheader():
-    return "exp\tcount"
+    return "exp\t%\tcount"
 def get_files(sample_name):
-    measurements = filter(lambda measurment: sample_name in measurment, os.listdir())
+    measurements = filter(lambda measurment: sample_name in measurment, os.listdir())   # only return files with sample number in name
+    measurements = filter(lambda measurment: "Ag" not    in measurment, measurements)   # don't include Ag files
     return sorted(list(measurements))
 def get_measurement():
     return 
 def make_stat(G):
-    mini=0
+    mini=1
     maxi=13
-    stat=np.repeat(0,maxi-mini)
-    stat_rel=np.repeat(0,maxi-mini)
+    stat=np.repeat(0,maxi)#-mini)
+    stat_rel=np.repeat(0,maxi)#-mini)
     n = len(G)
     for i in range(mini,maxi):
         #stat[i]=(np.absolute(np.around(np.log10(G))) == i).sum()
