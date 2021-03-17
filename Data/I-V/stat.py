d2a449e3a01cd09d795f001a7311faf2e97daff9
pur80a <j.w.dorn@gmx.at>
2021-03-17 22:38:37 +0100

    stat.py updated for output for EMMA

--- Data/I-V/stat.py
+++ Data/I-V/stat.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from math import ceil
 import os
 def get_sample_name():
     path=os.getcwd()
@@ -14,18 +15,42 @@ def get_conductance(filename):
             df=pd.read_csv(filename,"\t",header=r,names=("V","I","i","J","j"))
         except:
             pass
+            #print("could not read",filename)
         else:
 #            break
             i=0         #first 
             j=len(df)-1    #last
-            while i < j:
-                dv= (df["V"].iloc[j]-df["V"].iloc[i])
-                di= (df["I"].iloc[j]-df["I"].iloc[i])
-#                print(di/dv)
+#            print(j)
+            #TODO find pos at which V=0
+            zero=np.where( df["V"] == 0)
+            zero=ceil(j/2)
+#            print(df["V"][zero+1])
+#            print(zero )
+            # find prev 5-10 i and v and make avg 
+            # find past 5-10 i and v and make avg 
+            vdown=0
+            vup=0
+            idown=0
+            iup=0
+            d=2 #distance from zero
+            s=5 # how many for avg 
+#            while i < j:
+            for i in range(1,j):
+                if i+d+s < zero and i+s < zero:
+                    vdown=vdown + float(df["V"][i])
+                    idown=idown + float(df["I"][i])
+                if i-d-s < zero and i-s < zero:
+                    vup=vup + float(df["V"][i])
+                    iup=iup + float(df["I"][i])
+                dv= vup-vdown
+                di= iup-idown
+#                dv= (df["V"].iloc[j]-df["V"].iloc[i])
+#                di= (df["I"].iloc[j]-df["I"].iloc[i])
                 i=i+1
                 j=j-1
             R = dv/di
             G = di/dv
+#            print(G)
             return np.absolute(G)
     return 1    # if error with file reading
 def get_header():
@@ -37,11 +62,11 @@ def get_files(sample_name):
 #    print(all_files)
     measurements = filter(lambda measurment: sample_name in measurment, all_files)   # only return files with sample number in name
     measurements = filter(lambda measurment: "Ag" not    in measurment, measurements)   # don't include Ag files
+    #TODO: only highest 
     return sorted(list(measurements))
 def get_measurement():
     return 
 def make_stat(G):
-#    print(G)
     mini=1
     maxi=13
     stat=np.repeat(0,maxi)#-mini)
@@ -51,8 +76,22 @@ def make_stat(G):
         #stat[i]=(np.absolute(np.around(np.log10(G))) == i).sum()
         stat[i]=(np.absolute((np.log10(G)).astype(int)) == i).sum()
         stat_rel[i]=(np.absolute((np.log10(G)).astype(int)) == i).sum()/n*100
-        print(i,stat_rel[i],stat[i],sep="\t")
-    return stat
+        ### old visualization stat
+        # print(i,stat_rel[i],stat[i],sep="\t")
+    #return stat
+    ########################################################
+    msd=0   #mean square distance
+    g0=13
+    holes=0
+    for g in G:
+        lgg = np.log10(g)
+        if lgg < 5:
+            holes = holes + 1
+        sd=(g0-lgg)
+        msd=msd+sd
+    msd=msd/n
+    return msd,holes
+    
 
 #######################
 def main():
@@ -65,8 +104,9 @@ def main():
         G=get_conductance(filename)
         Gs.append(G)
     #    print(filename,get_conductance(filename),sep="\t")
-    print(get_Gheader())
-    make_stat(Gs)
+#    print(get_Gheader())
+    msd,holes=make_stat(Gs)
+    print(sample_name,msd, holes, sep="\t")
     #print(Gs)
 
     #print(get_sample_name())
