21838996c7abd621d401d8f64a71ebb6e9280438
pur80a <j.w.dorn@gmx.at>
2021-03-26 12:16:25 +0100

    managed to load data and make to np.array

--- Code/ML/fit.py
+++ Code/ML/fit.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np 
-import sklearn  as skl
+import sklearn.svm  as skl
 import pandas as pd
 
 infile="stat.dat"
@@ -15,7 +15,29 @@ G = df.conductivity
 phd = df.phdensity
 layers = df.layers
 Cal_vel = df.Cal_vel
-print(df)
+
+tmin=0                      # train minimum index
+tmax=20
+ttmin =tmax                 # test minimum index
+ttmax=25
+trainx1 = conc[tmin:tmax].to_numpy()
+trainx2 = layr[tmin:tmax]
+trainy1 = G[tmin:tmax]
+trainy2 = phd[tmin:tmax]
+testx1 = conc[ttmin:ttmax]
+testx2 = layr[ttmin:ttmax]
+testy1 = G[ttmin:ttmax]
+testy2 = phd[ttmin:ttmax]
+trainx = np.array([trainx1, trainx2 ])
+#trainx1 =trainx1.to_numpy()
+print(trainx1)
+trainx1.reshape(np.array(trainx1),1)
+print(trainx1.shape())
+
+ML1 = skl.LinearSVR()
+#ML1.fit(trainx,trainy1)
+
+#print(df)
 #print(phd)
 #print(df.conc)
 #print(df["nr"])
