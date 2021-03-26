210735f5feb9552d707900e266e80223f27fbc38
pur80a <j.w.dorn@gmx.at>
2021-03-26 12:41:29 +0100

    minimal working ML

--- Code/ML/fit.py
+++ Code/ML/fit.py
@@ -21,21 +21,29 @@ tmax=20
 ttmin =tmax                 # test minimum index
 ttmax=25
 trainx1 = conc[tmin:tmax].to_numpy()
-trainx2 = layr[tmin:tmax]
-trainy1 = G[tmin:tmax]
-trainy2 = phd[tmin:tmax]
-testx1 = conc[ttmin:ttmax]
-testx2 = layr[ttmin:ttmax]
-testy1 = G[ttmin:ttmax]
-testy2 = phd[ttmin:ttmax]
-trainx = np.array([trainx1, trainx2 ])
+trainx2 = layr[tmin:tmax].to_numpy()
+trainy1 = G[tmin:tmax].to_numpy()
+trainy2 = phd[tmin:tmax].to_numpy()
+testx1 = conc[ttmin:ttmax].to_numpy()
+testx2 = layr[ttmin:ttmax].to_numpy()
+testy1 = G[ttmin:ttmax].to_numpy()
+testy2 = phd[ttmin:ttmax].to_numpy()
+#trainx = np.array([trainx1, trainx2 ]).reshape(20,2)
+trainx = np.transpose( np.array([trainx1, trainx2 ]) )
+testx = np.transpose( np.array([testx1, testx2 ]) )
+trainy = trainy1    #.reshape(20,1)
+testy = testy1    #.reshape(20,1)
 #trainx1 =trainx1.to_numpy()
-print(trainx1)
-trainx1.reshape(np.array(trainx1),1)
-print(trainx1.shape())
+#print(trainx1)
+#trainx1.reshape(np.array(trainx1),1)
+#print(type(trainx1))
+print(trainx.shape)
+print(trainy.shape)
 
 ML1 = skl.LinearSVR()
-#ML1.fit(trainx,trainy1)
+ML1.fit(trainx,trainy)
+pred_testy = ML1.predict(testx)
+print(np.array([ testy, pred_testy ] ) ) 
 
 #print(df)
 #print(phd)
