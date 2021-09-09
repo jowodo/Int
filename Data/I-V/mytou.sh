f9f0ce74344957cbcced021a98acc1a5b66176e9
J <cube@bor.local>
2021-09-09 19:57:52 +0200

    fixed mytu.sh script "has to be used with zsh!"

--- Data/I-V/mytou.sh
+++ Data/I-V/mytou.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/zsh
 
 for dir in I-V*;  do
+#for dir in I-V_151*;  do
     pushd $dir
     for file in  *L*C*; do 
-        sed -i "s/µ/u/" $file
+#    for file in  *L1C1_04; do 
+        sed "s/µ/u/" <<< $( iconv -f ISO-8859-1 -t UTF-8 $file)  > $file
     done
     popd
 done
