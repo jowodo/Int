e5e28d17f4d64b702c4f62aa33c90102e23f5da8
pur80a <ziffernsumme11@gmail.com>
2022-10-21 19:17:44 +0200

    edit scripts

--- Code/Statistics/scripts/anova.R
+++ Code/Statistics/scripts/anova.R
@@ -10,13 +10,37 @@ dat<-read.delim("../db_final.tsv", colClasses=c(n,n,n,n,n,n,f,f,f,f,f,f))
 d.all <- dat %>% select (! avg1.G.:avg2.G.)
 d.emma <- d.all %>% filter ( enr != 0 ) 
 
+G.TCal.emma.aov<-aov(G ~ TCal, data = d.emma)
+print("ANOVA with EMMA samples: G ~ TCal")
+summary(G.TCal.emma.aov)
+
 G.TCal.all.aov<-aov(G ~ TCal, data = d.all)
-print("ANOVA with all samples")
+print("ANOVA with all samples: G ~ TCal")
 summary(G.TCal.all.aov)
 
-G.TCal.emma.aov<-aov(G ~ TCal, data = d.emma)
-print("ANOVA with EMMA samples")
-summary(G.TCal.emma.aov)
+G.layr.emma.aov<-aov(G ~ layr, data = d.emma)
+print("ANOVA with EMMA samples: G ~ layr")
+summary(G.layr.emma.aov)
+
+G.layr.all.aov<-aov(G ~ layr, data = d.all)
+print("ANOVA with all samples: G ~ layr")
+summary(G.layr.all.aov)
+
+G.TDOC.emma.aov<-aov(G ~ TDOC, data = d.emma)
+print("ANOVA with EMMA samples: G ~ TDOC")
+summary(G.TDOC.emma.aov)
+
+G.TDOC.all.aov<-aov(G ~ TDOC, data = d.all)
+print("ANOVA with all samples: G ~ TDOC")
+summary(G.TDOC.all.aov)
+
+G.TvDOC.emma.aov<-aov(G ~ TDOC:vDOC, data = d.emma)
+print("ANOVA with EMMA samples: G ~ TDOC:vDOC")
+summary(G.TvDOC.emma.aov)
+
+G.TvDOC.all.aov<-aov(G ~ TDOC:vDOC, data = d.all)
+print("ANOVA with all samples: G ~ TDOC:vDOC")
+summary(G.TvDOC.all.aov)
 
 
 
---
+++ Code/Statistics/scripts/emma.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+#
+# USAGE: execute script from within the folder
+
+library("dplyr")
+load("../../EMMA/t5") 
+source("../../EMMA/inter.R") 
+#for (i in 1:4){
+#    emmacheck(t4,graph=1,eresp=i)
+#}
+#print("Global Best")
+#t5$Gb.arch
+#print("Personal Best")
+##matrix(t5$Pb.arch,nrow=5)
+##t5$xpop[:2] 
+
+x<-t4
+input <- cbind(x$xpop,x$ypop)
+fmla <- as.formula(paste(paste("data.matrix(input[,",ncol(x$xpop)+1,":",ncol(x$xpop)+ncol(x$ypop),"])"),"~", paste(colnames(x$xpop),collapse= "+")))
+mars.mod <- earth(fmla,degree=2,trace=0,minspan=1,data=input,keepxy=TRUE)
+head(x$ypop,1)
+format(mars.mod)
+
+
+
