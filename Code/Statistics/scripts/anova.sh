85f66018a3d5ef9ca393f0edbc2d2cb184d5c88f
pur80a <ziffernsumme11@gmail.com>
2022-11-15 20:25:12 +0100

    anova only G and phd

--- Code/Statistics/scripts/anova.sh
+++ Code/Statistics/scripts/anova.sh
@@ -2,7 +2,7 @@
 #
 #USAGE: ./anova.sh [--all] 
 
-for dep in G phd layr vcal 
+for dep in G phd # layr vcal 
 do 
     echo $dep
     ./anova-${dep}.R $1 | grep -A1 --no-group-separator -e "F val" | grep "*" 2> /dev/null
