33def5266cf299273beb24fe550cd1516d3da227
pur80a <ziffernsumme11@gmail.com>
2022-11-30 22:04:45 +0100

    GS finished?

--- Code/ML/2022/0_gs.sh
+++ Code/ML/2022/0_gs.sh
@@ -1,15 +1,22 @@
 #!/bin/bash 
 OUTFILE=gs_best.out
-source ../../../../pyvenv/bin/activate
-./gs_krr.py -e -y2 2>/dev/null > gs_krr.g.emma # grid search with only EMMA samples as data set
-./gs_krr.py -e -y3 2>/dev/null > gs_krr.p.emma # grid search with only EMMA samples as data set
-./gs_krr.py    -y2 2>/dev/null > gs_krr.g.all # grid search with all samples as data set
-./gs_krr.py    -y3 2>/dev/null > gs_krr.p.all # grid search with all samples as data set
-./gs_svm.py -e -y2 2>/dev/null > gs_svm.g.emma 
-./gs_svm.py -e -y3 2>/dev/null > gs_svm.p.emma 
-./gs_svm.py    -y2 2>/dev/null > gs_svm.g.all
-./gs_svm.py    -y3 2>/dev/null > gs_svm.p.all
-
+# IF THERE IS A COMMAND LINE ARGUMENT
+if [ "x$1" != "x" ] 
+then 
+    # IF IT IS "-r", then recalculate
+    if [ "$1" = "-r" ] 
+    then
+        source ../../../../pyvenv/bin/activate
+        ./gs_krr.py -e -y2 2>/dev/null > gs_krr.g.emma # grid search with only EMMA samples as data set
+        ./gs_krr.py -e -y3 2>/dev/null > gs_krr.p.emma # grid search with only EMMA samples as data set
+        ./gs_krr.py    -y2 2>/dev/null > gs_krr.g.all # grid search with all samples as data set
+        ./gs_krr.py    -y3 2>/dev/null > gs_krr.p.all # grid search with all samples as data set
+        ./gs_svm.py -e -y2 2>/dev/null > gs_svm.g.emma 
+        ./gs_svm.py -e -y3 2>/dev/null > gs_svm.p.emma 
+        ./gs_svm.py    -y2 2>/dev/null > gs_svm.g.all
+        ./gs_svm.py    -y3 2>/dev/null > gs_svm.p.all
+    fi
+fi 
 
 rm $OUTFILE
 for file in $(ls gs*emma) 
