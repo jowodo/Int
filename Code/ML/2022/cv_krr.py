6cf9cee998d9dcffb2f437828a7b5a2aa9c0ba2d
pur80a <ziffernsumme11@gmail.com>
2022-12-04 14:24:04 +0100

    clean cross validation

---
+++ Code/ML/2022/cv_krr.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import numpy as np 
+#from sklearn.svm import SVR # Epsilon-support Vector Regression
+from sklearn.kernel_ridge import KernelRidge as KRR
+import sklearn
+import pandas as pd
+import dfply
+import argparse
+#
+#################
+# KERNEL FOR SVM
+kernel=["poly","rbf","sigmoid"] #HPGS
+# DEGREE OF POLYNOMIAL; ignored by rest
+degree=range(1,4) #HPGS
+# some kind of scaling factor; can be set to auto/scale, include into list? 
+gamma=[0.01, 0.05, 0.1, 0.5, 1.0, 5, 10] #HPGS
+# alpha: bigger alpha, bigger regularisation 
+alpha=[0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2] #, 10, 50, 100] #HPGS
+#
+# MAKE COMMAND LINE ARGUMENTS
+parser = argparse.ArgumentParser(description ='Grid Search for hyper parameters')
+parser.add_argument('-e', '--emma', dest ='only_emma',action ='store_true', help ='only use emma data')
+parser.add_argument('-k', '--kfold', dest ='kfold',action ='store', default='5', help ='Split into k set for k-fold cross validation') 
+parser.add_argument('-m', '--maxiter', dest ='maxiter',action ='store', default='10000', help ='Hard limit on itermations within solver, default: 10000, use -1 for unlimited') 
+parser.add_argument('-t', '--test', dest ='test',action ='store_true', help ='Test: Only use subset of hyperparameters') 
+parser.add_argument('-x', '--scale', dest ='scale', action ='store_true', default=False, help='Scale input before fitting')
+parser.add_argument('-y', '--yindex', dest ='y_index',action ='store', choices={'2','3','7','10'}, default='2', help ='y to fit: 2=G; 3=phd; 7=layers; 10=vCal [default=2]')
+args = parser.parse_args()
+#
+# GET INPUT AND OUTPUT DATA
+def get_sets(df,names,args):
+    data=np.array(df)
+    X=data[:,6:]
+    # MUTLIply by 60 bcs C/h instead of C/min
+    X[:,4]=X[:,4]*60
+    X_names=names[6:]
+    Y=data[:,int(args.y_index)].reshape(-1)
+    return X,Y
+#
+#LOAD DATA FROM FILE 
+infile="../../Statistics/db_final.tsv"
+#         0     1       2               3           4          5          6       7        8        9       10      11 
+names = ["nr", "enr", "conductivity", "phdensity", "avg1(G)", "avg2(G)", "conc", "layers", "vDOC", "TDOC", "vCal", "TCal"]
+df=pd.read_csv(infile, skiprows=1, delim_whitespace=True, names = names)
+# FILTER OUT NON EMMA SAMPLES
+if args.only_emma:
+    df = df >>dfply.mask(df.enr !=0)
+#
+#PREPROCESSING DATA 
+X,Y=get_sets(df,names,args)
+if args.scale : X=sklearn.preprocessing.StandardScaler().fit_transform(X)
+#
+# temp short test
+if args.test : 
+    degree=[1,2]; gamma=[0.01,0.1]; alpha=[0.5,0.1]; 
+n_exps=len(alpha)*(len(degree)+len(kernel)-1)*len(gamma)
+#
+# HEADER LINE 
+print("%s accuracy with standard deviation", names[int(args.y_index)])
+i=1
+#print(hline)
+for k in kernel:
+    if k=="poly":
+        for d in degree:
+            for a in alpha:
+                for g in gamma:
+                    line=""
+                    ML = KRR(kernel=k, alpha=a, degree=d, gamma=g)
+                    score=sklearn.model_selection.cross_val_score(ML,X,Y,cv=int(args.kfold))
+                    line=f"{score.mean():.6f}"+"\t"+ f"{score.std():.6f}"+"\t"
+                    print(line+ k+ 
+                            "\tdeg="+str(d)+ 
+                            "\ta="+f"{a:.2f}"+ 
+                            "\tg="+str(g)+ 
+                            "\t"+str(i)+"/"+str(n_exps), 
+                            flush=True)
+                    i+=1
+    else:
+        for a in alpha:
+            for g in gamma:
+                line=""
+                ML = KRR(kernel=k, alpha=a, degree=d, gamma=g)
+                score=sklearn.model_selection.cross_val_score(ML,X,Y,cv=int(args.kfold))
+                line=f"{score.mean():.6f}"+"\t"+ f"{score.std():.6f}"+"\t"
+                print(line+ k+ 
+                        "\tdeg="+str(0)+ 
+                        "\ta="+f"{a:.2f}"+ 
+                        "\tg="+str(g)+ 
+                        "\t"+str(i)+"/"+str(n_exps), 
+                        flush=True)
+                i+=1
---
+++ Code/ML/2022/cv_svm.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import numpy as np 
+from sklearn.svm import SVR # Epsilon-support Vector Regression
+#from sklearn.kernel_ridge import KernelRidge as KRR
+import sklearn
+import pandas as pd
+import dfply
+import argparse
+#
+# KERNEL FOR SVM
+kernel=["poly","rbf","sigmoid"] # HPGS
+# regularization parameter
+C= np.array(range(1,20))*0.05 # HPGS
+# DEGREE OF POLYNOMIAL; ignored by rest
+degree=range(1,4) # HPGS
+# epsilon-tube size for no penalty; rather large 
+epsilon= np.array(range(1,20))*0.2 # HPGS
+# some kind of scaling factor; can be set to auto/scale, include into list? 
+gamma=[0.01, 0.05, 0.1, 0.5, 1.0, 5, 10, "scale"] # HPGS
+#
+# MAKE COMMAND LINE ARGUMENTS
+parser = argparse.ArgumentParser(description ='Grid Search for hyper parameters')
+parser.add_argument('-e', '--emma', dest ='only_emma',action ='store_true', help ='only use emma data')
+parser.add_argument('-k', '--kfold', dest ='kfold',action ='store', default='5', help ='Split into k set for k-fold cross validation') 
+parser.add_argument('-m', '--maxiter', dest ='maxiter',action ='store', default='10000', help ='Hard limit on itermations within solver, default: 10000, use -1 for unlimited') 
+parser.add_argument('-t', '--test', dest ='test',action ='store_true', help ='Test: Only use subset of hyperparameters') 
+parser.add_argument('-x', '--scale', dest ='scale', action ='store_true', default=False, help='Scale input before fitting')
+parser.add_argument('-y', '--yindex', dest ='y_index',action ='store', choices={'2','3','7','10'}, default='2', help ='y to fit: 2=G; 3=phd; 7=layers; 10=vCal [default=2]')
+args = parser.parse_args()
+#
+# GET INPUT AND OUTPUT DATA
+def get_sets(df,names,args):
+    data=np.array(df)
+    X=data[:,6:]
+    # MUTLIply by 60 bcs C/h instead of C/min
+    X[:,4]=X[:,4]*60
+    X_names=names[6:]
+    Y=data[:,int(args.y_index)].reshape(-1)
+    return X,Y
+#
+#LOAD DATA FROM FILE 
+infile="../../Statistics/db_final.tsv"
+names = ["nr", "enr", "conductivity", "phdensity", "avg1(G)", "avg2(G)", "conc", "layers", "vDOC", "TDOC", "vCal", "TCal"]
+#         0     1       2               3           4          5          6       7        8        9       10      11 
+df=pd.read_csv(infile, skiprows=1, delim_whitespace=True, names = names)
+# FILTER OUT NON EMMA SAMPLES
+if args.only_emma:
+    df = df >>dfply.mask(df.enr !=0)
+#
+#PREPROCESSING DATA 
+X,Y=get_sets(df,names,args)
+if args.scale : X=sklearn.preprocessing.StandardScaler().fit_transform(X)
+#
+# temp short test
+if args.test : 
+    C=[0.5,0.1]; degree=[1,2]; epsilon=[1,2]; gamma=["scale",0.1] # ,"auto"]
+n_exps=len(C)*(len(degree)+len(kernel)-1)*len(epsilon)*len(gamma)
+#
+# HEADER LINE 
+print("%s accuracy \tstandard deviation" % names[int(args.y_index)])
+i=1
+for k in kernel:
+    if k=="poly":
+        for d in degree:
+            for c in C:
+                for e in epsilon:
+                    for g in gamma:
+                        line=""
+                        ML = SVR(kernel=k, C=c, degree=d, epsilon=e, gamma=g, max_iter=int(args.maxiter))
+                        score=sklearn.model_selection.cross_val_score(ML,X,Y,cv=int(args.kfold))
+                        line=f"{score.mean():.6f}"+"\t"+ f"{score.std():.6f}"+"\t"
+                        print(line+ k+ 
+                              "\tdeg="+str(d)+ 
+                              "\tC="+f"{c:.1f}"+ 
+                              "\te="+f"{e:.1f}"+
+                              "\tg="+str(g)+ 
+                              "\t"+str(i)+"/"+str(n_exps), 
+                              flush=True)
+                        i+=1
+    else:
+        for c in C:
+            for e in epsilon:
+                for g in gamma:
+                    line=""
+                    score=sklearn.model_selection.cross_val_score(ML,X,Y,cv=int(args.kfold))
+                    line=f"{score.mean():.6f}"+"\t"+ f"{score.std():.6f}"+"\t"
+                    print(line+ k+ 
+                          "\tdeg="+str(0)+ 
+                          "\tC="+f"{c:.1f}"+ 
+                          "\te="+f"{e:.1f}"+
+                          "\tg="+str(g)+ 
+                          "\t"+str(i)+"/"+str(n_exps), 
+                          flush=True)
+                    i+=1
