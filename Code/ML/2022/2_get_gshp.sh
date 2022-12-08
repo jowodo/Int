fd54ee3da941ac8cb6e1b81ffa5e536796d7aae0
pur80a <ziffernsumme11@gmail.com>
2022-12-08 18:50:10 +0100

    post-emma table

---
+++ Code/ML/2022/2_get_gshp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+#
+#GET HYPER PARAMETERS FOR GRID SEARCH 
+echo "Hyper parameter for grid search"
+grep --color=auto HPGS gs*py
--- Code/ML/2022/get_gshp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-#
-grep --color=auto HPGS gs*py
